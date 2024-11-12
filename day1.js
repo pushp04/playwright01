a460a5d3060b306527858ef8d0f95588655c3447
Pushp Choudhery <pchoudhery@scigon.com>
2024-11-12 16:18:56 +0530

    my first commit

---
+++ day1.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let a=10
+
+let b= "apple"
+
+let c="banana"
+
+let d= 20
+function concatenate(first, last) {
+    var full;
+    full = first + last;
+    return full;
+ }
+
+ const num = new Number(20);
+
+ value : Number.MAX_VALUE
+
+let nam =concatenate(a , d)
+
+console.log(1=="1")
+// console.log(a>b)
+// console.log(nam)
+
---
+++ hello.js
@@ -0,0 +1,10 @@
+
+const a=10
+
+a=20;
+
+let b=10
+
+// var c=10
+
+console.log("Hello Javascript")
