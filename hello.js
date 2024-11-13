bd0b82ce9e4c0f93be95ddcc5888a5174af57b17
Pushp Choudhery <pchoudhery@scigon.com>
2024-11-13 17:22:35 +0530

    day2 updates

--- hello.js
+++ hello.js
@@ -1,10 +1,43 @@
 
-const a=10
 
-a=20;
+let a=20;
 
-let b=10
+let b=3
 
+let s=null;
+
+let c =true
+
+let d= "Hello Java script"
+let str='how are you'
+
+// let s=new String('hello javascript')
+
+let e=4223313135135n
+let f=null
+
+let g=[7,2,1,5]
+
+console.log(b--)
+a++,++a
+a=a+1;
+
+a--,--a
+a=a-1;
+
+console.log(b)
+
+
+
+// console.log(d)
+// console.log(e)
+
+// console.log(b)
+// console.log(B)
+// g[0]=20;
 // var c=10
-
-console.log("Hello Javascript")
+// console.log('100'+Number(f))
+// console.log(typeof(d.split(" ")))
+// console.log(g.sort())
+// console.table([typeof(a),typeof (b),typeof(c),typeof d,typeof e,typeof f])
+// console.log("Hello Javascript")
