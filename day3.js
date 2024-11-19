e64da8aeb7f3f1106facf532f92bb653dc1d4f5f
Pushp Choudhery <pchoudhery@scigon.com>
2024-11-20 00:39:57 +0530

    day4

---
+++ day3.js
@@ -0,0 +1,46 @@
+let a=20;
+
+let b=3
+
+let s=null;
+
+let c =true
+
+let d= "Hello Java script"
+let str='how are you'
+console.log(str[0]);
+let e=4223313135135n
+let f=null
+
+let g=[7,2,1,5]
+
+let aa=2;
+let bb=2;
+let cc=3;
+let dd=3
+//T&&T= T,
+//T&&F=F,
+//F&&F=F,
+
+let xst= (aa!=bb) ? "Equals":"NotEquals";
+let i=5;
+let h=15;
+
+let marks=150
+//A B F
+if (marks<30) 
+{
+    console.log("F")
+} 
+if(marks<70){
+    console.log("B")
+}
+if(marks<100){
+    console.log("A")
+}
+else
+{
+    console.log("Extra ordinary Student")
+}
+
+
---
+++ day4.js
@@ -0,0 +1,91 @@
+// WAP to print nummber from 20 to 1 decrement by 2
+// 20 18 16....
+
+
+// for (let index = 1; index <= 10; index++) 
+// {
+//     console.log(index);
+// }
+
+// index-10,9...1
+// o/p -20,18...2
+
+// let index=10
+
+// while (index>0) 
+// {
+//     console.log("while loop--"+index);
+//     index--;
+    
+// }
+
+
+
+// let count = 0;
+//         console.log("Starting Loop");
+//         while (count < 10) {
+//             console.log("Current Count : " + count );
+//             count++;
+//         }
+//         console.log("Loop stopped!");
+
+// let index=1;
+// do {
+//     index+=1;
+//     console.log(index)
+
+// } while (index>2);
+        
+// let count = 0;
+//         console.log("Starting Loop")
+//         do {
+//             console.log("Current Count : " + count );
+//             count++;
+//         }
+//         while (count < 5);
+//         console.log("Loop stopped!");
+
+// for (let index = 1; index <= 10; index++) 
+//     {
+//         if(index>8){
+//             break;
+//         }
+//         console.log(index);
+//     }
+
+// for (let index = 1; index <= 10; index++) 
+//         {
+//             if(index==5){
+//                 continue;
+//             }
+//             console.log("loop value ---"+index);
+//         }
+
+
+// let input =1;
+// switch (input) {
+//         case 1:console.log("Mon")
+        
+//         break;
+//         case 2:console.log("tue")
+        
+//         break;
+//         case 3:console.log("wed")
+        
+//         break;
+//         case 4:console.log("thur")
+        
+//         break;
+//         case 5:console.log("fri")
+        
+//         break;
+//         case 6:console.log("sat")
+        
+//         break;
+//         case 7:console.log("sun")
+        
+//         break;
+
+//     default: console.log("invalid entry")
+//         break;
+// }
