04e32f25deae8dbb288a61a26024d8c7ce90fcb1
Pushp Choudhery <pchoudhery@scigon.com>
2024-11-22 00:36:13 +0530

    day5.1

---
+++ day5.js
@@ -0,0 +1,80 @@
+// let person={
+//     fname :"john",
+//     lname : "doe",
+//     age : 30,
+//     isStudent : false
+// };
+
+// Object.keys(person).forEach(key=>{
+//     console.log(key+"----"+person[key])
+// })
+
+
+// for (let key in person) {
+//    console.log(key+"----"+person[key])
+// }
+
+
+function func() {
+console.log( "i am a reusable function");
+console.log( "i am a reusable function");
+console.log( "i am a reusable function");
+console.log( "i am a reusable function");
+console.log( "i am a reusable function");
+}
+
+func()
+
+// function sum(a,b){
+//     z=a+b;
+//     console.log("Sum of a and b is--"+z)
+// }
+// let a=2
+// let b=3
+
+// sum(a,b)
+// sum(7,9)
+
+
+// c=2;
+// d=3;
+// console.log(c+d)
+
+// e=5
+// f=7
+// console.log(e+f)
+
+// g=5
+// h=3
+// console.log(g+h)
+
+// let sum = function (x, y) {
+//     return x + y;
+//     };
+
+//     let z = sum(1,6);
+//     console.log(z); 
+    
+// const sum = function addNumbers(x, y) {
+//     return x + y;
+//     };
+    
+//     let z = sum(2, 3);
+//     console.log(z)
+    
+
+// const mul = function (a, b) {
+//     return a * b;
+//     };
+
+//     let result = mul(4, 5);
+//     console.log(result)
+    
+function sayHello(name, age) {
+console.log(name + " is " + age + " years old.");
+}
+
+
+let nam="pushp";
+let age =30;
+sayHello(nam, age);
