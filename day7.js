c9e3a073219d708be9ec219836b70e3321da8870
Pushp Choudhery <pchoudhery@scigon.com>
2024-12-04 00:29:51 +0530

    day7

---
+++ day7.js
@@ -0,0 +1,13 @@
+let val= new String("Hello");
+                    
+let s1= "this is java script";
+let s2="     Java     "
+// console.log(typeof val)
+// console.log(val[0])
+// console.log(typeof (s1.concat(val)))
+
+let b= s1.endsWith("pt");
+// console.log(s1.endsWith("pt"))
+// let c=!b;
+// console.log(s1.includes(s2))
+console.log(s1.toLowerCase())
