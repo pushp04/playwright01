886cbc7f18874550010e7cfee3b1cf88526fbc4f
Pushp Choudhery <pchoudhery@scigon.com>
2024-12-06 00:37:40 +0530

    day8

--- day7.js
+++ day7.js
@@ -10,4 +10,57 @@ let b= s1.endsWith("pt");
 // console.log(s1.endsWith("pt"))
 // let c=!b;
 // console.log(s1.includes(s2))
-console.log(s1.toLowerCase())
+// console.log(s1.toLowerCase())
+
+// function Car(brand) { // Constructor function
+// 	this.brand = brand; // property initialization
+// }
+// const carObj = new Car("Audi");
+// console.log(carObj)
+
+// class Car {
+//     constructor() {
+//         this.brand = "BMW";
+//         this.model = "X5";
+//         this.year = 2019;
+//     }
+// }
+// // instantiate myCar object
+// 	const myCar = new Car();
+// // display the properties
+// console.log(myCar.brand);
+// console.log(myCar.model);
+// console.log( myCar.year);
+
+// class Car {
+//     constructor(brand,model,year) {
+//     this.brand =brand;
+//     this.model =model;
+//     this.year =year;
+//      }
+//     }
+//     // instantiate myCar object
+//     const myCar1 = new Car("BMW","X5", 2019);
+//     const myCar = new Car("VW","Polo", 2022);
+//     // display the properties
+//     console.log(myCar.brand);
+//     console.log(myCar.model);
+//     console.log( myCar.year);
+
+
+    class Car {
+        constructor(brand, price) {
+            this.brand = brand;
+            this.price = price;
+        }
+
+        updateprice(newPrice) {
+        this.price = newPrice;
+        }
+    }
+    
+
+    const myCar = new Car("BMW",9800000);
+		
+	myCar.updateprice(8800000); // updating price
+    
---
+++ day8.js
@@ -0,0 +1,61 @@
+// let arr =new Array();
+
+// for (let index = 0; index < 10; index++) {
+//     arr[index]= index*2;
+// }
+
+// console.log(arr);
+
+// let ar=[1,3,6,3,7,2]
+// console.log(ar)
+// ar[0]=9
+// console.log(ar)
+// const arr1 = [10, 40, 50, 60, 80, 90]; // Array of numbers const 
+// let arr2 = ["Hello", "Hi", "How", "are", "you?"]; // Array of strings const 
+// let arr3 = [true, false, true, true]; // Array of booleans
+// console.table([arr1,arr2,arr3])
+
+// for (const element of arr2) {
+//     console.log(element+"  qwertyui")
+// }
+
+// arr2.push("ola")
+// arr2[arr2.length]=  "amigos";
+// console.log(arr2.length)
+
+// let arr =new Array();
+
+// let i=0;
+// while (i<arr2.length) {
+//     console.log(typeof arr2[i]);
+//     i++;
+//     break;
+// }
+// console.log(arr)
+
+// let arr2 = ["How", "are", "you?","Hello", "Hi"]; // Array of strings const 
+// arr2.pop();
+// console.log(arr2)
+// arr2.pop();
+// console.log(arr2)
+// arr2.pop("Hi")
+
+// let arr=[4,7,2,9,5]
+// let ar =['a','g','d','t','f']
+// console.log(arr2)
+// console.log(arr2.sort())
+// console.log(arr2.reverse())
+
+// console.log((arr.concat(ar)))
+// console.log((arr.concat(ar)).sort())
+
+// let date = new Date();// Date and Time in UTC
+// const date = new Date("August 19, 2024 09:30:54");
+// let date = new Date(999999999999);
+// let date = new Date(-999999999999);
+// const date = new Date(2001, 5, 14, 6, 43, 58, 342);
+// console.log(date.toUTCString())
+
+
+
+
